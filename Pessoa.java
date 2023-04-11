f1449b8eb0a0dd110c053289fa1820df73635a61
YasminGon <yasmingon08@hotmail.com>
2023-04-11 14:10:31 +0000

    gravando

---
+++ Pessoa.java
@@ -0,0 +1,23 @@
+Public class Pessoa{
+
+    private String nome;
+    private int idade;
+    private String celular;
+    private String cpf;
+
+public String getNome(){
+    return this.nome;
+}
+public String setNome(String nome){
+    this.nome=nome;
+}
+public int getIdade(){
+    return this.idade;
+}
+public void setIdade (int idade){
+    this.idade=idade;
+}
+public String getCelular(){
+    return this.celular;
+}
+
---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome(Bia);
+        system.out.println(pessoa.getNome());
+        pessoa.setIdade(16);
+        system.out.println (pessoa.getIdade());
+
+    }
+}
